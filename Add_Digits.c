1a1bc68e557d8f892541fac70742123f2cfae77b
navi0307 <97536148+navi0307@users.noreply.github.com>
2022-01-24 10:11:39 +0530

    #include<stdio.h> int main() {     int a,i=0,j;     scanf("%d",&a);     while(a>0||i>9)     {         if(a==0)         {             a=i;             i=0;         }         j=a%10;         i+=j;         a/=10;     }     printf("%d",i);     return 0; }

---
+++ Add_Digits.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,i=0,j;
+    scanf("%d",&a);
+    while(a>0||i>9)
+    {
+        if(a==0)
+        {
+            a=i;
+            i=0;
+        }
+        j=a%10;
+        i+=j;
+        a/=10;
+    }
+    printf("%d",i);
+    return 0;
+}
