2eba985e70258f2c4cbd5ed63163624950e56d09
navi0307 <97536148+navi0307@users.noreply.github.com>
2022-04-27 01:49:36 +0530

    #include<stdio.h> int main() {     int n,r,sum=0,p=1,t;     scanf("%d",&n);     t=n;     while(n>0)     {         r=n%10;         sum+=r;         p*=r;         n/=10;     }         if(sum==p)         printf("Spy Number");         else         printf("Not Spy Number");         return 0; }

---
+++ Spy_Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,sum=0,p=1,t;
+    scanf("%d",&n);
+    t=n;
+    while(n>0)
+    {
+        r=n%10;
+        sum+=r;
+        p*=r;
+        n/=10;
+    }
+        if(sum==p)
+        printf("Spy Number");
+        else
+        printf("Not Spy Number");
+        return 0;
+}
