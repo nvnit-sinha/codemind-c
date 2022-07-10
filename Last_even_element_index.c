5a3e39e06fb9168ea2f0132d8cddd5069d935fc6
navi0307 <97536148+navi0307@users.noreply.github.com>
2022-07-10 20:35:02 +0530

    #include<stdio.h> int main(){     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     for(i=n-1;i>=0;i--){         if(a[i]%2==0)         break;     }     printf("%d",i);     return 0; }

---
+++ Last_even_element_index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i--){
+        if(a[i]%2==0)
+        break;
+    }
+    printf("%d",i);
+    return 0;
+}
