00b2c23c7fff1de825f516440cf11f3273683ff1
navi0307 <97536148+navi0307@users.noreply.github.com>
2022-10-16 04:34:05 +0530

    #include<iostream> using namespace std; int main(){     int n;      cin>>n;      int arr[n];      int sum=0,sum1 = 0, sum2=0;     for(int i=0;i<n;i++){         cin>>arr[i];         sum+=arr[i];     }
        for(int i=0;i<n/2;i++){
            sum1+=arr[i];
        }
        
        sum2 = sum - sum1;
        
        cout<<sum1<<endl<<sum2;
    }

---
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,20 @@
+#include<iostream>        
+using namespace std;        
+int main(){
+    int n;
+     cin>>n;
+     int arr[n];
+     int sum=0,sum1 = 0, sum2=0;
+    for(int i=0;i<n;i++){     
+        cin>>arr[i];   
+        sum+=arr[i];
+    }
+    
+    for(int i=0;i<n/2;i++){
+        sum1+=arr[i];
+    }
+    
+    sum2 = sum - sum1;
+    
+    cout<<sum1<<endl<<sum2;
+}
